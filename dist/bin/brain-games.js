73a55a004534f85677c24f1de1ee2d2831e4387d
botirk <antipchel@gmail.com>
2019-05-01 16:17:52 +0300

    travis cl integration try#2

--- dist/bin/brain-games.js
+++ dist/bin/brain-games.js
@@ -1,8 +1,10 @@
 #!/usr/bin/env node
 "use strict";
 
-var _index = require("./../index.js");
+var _index = _interopRequireDefault(require("../index"));
 
-console.log("Welcome to the Brain Games!");
-(0, _index.questionNameAndSayHello)();
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9iaW4vYnJhaW4tZ2FtZXMuanMiXSwibmFtZXMiOlsiY29uc29sZSIsImxvZyJdLCJtYXBwaW5ncyI6IkFBQUE7OztBQUVBOztBQUVBQSxPQUFPLENBQUNDLEdBQVIsQ0FBWSw2QkFBWjtBQUNBIiwic291cmNlc0NvbnRlbnQiOlsiIyEvdXNyL2Jpbi9lbnYgbm9kZVxuXG5pbXBvcnQgeyBxdWVzdGlvbk5hbWVBbmRTYXlIZWxsbyB9IGZyb20gJy4vLi4vaW5kZXguanMnO1xuXG5jb25zb2xlLmxvZyhcIldlbGNvbWUgdG8gdGhlIEJyYWluIEdhbWVzIVwiKTtcbnF1ZXN0aW9uTmFtZUFuZFNheUhlbGxvKCk7Il19
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+console.log('Welcome to the Brain Games!');
+(0, _index.default)();
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9iaW4vYnJhaW4tZ2FtZXMuanMiXSwibmFtZXMiOlsiY29uc29sZSIsImxvZyJdLCJtYXBwaW5ncyI6IkFBQUE7OztBQUVBOzs7O0FBRUFBLE9BQU8sQ0FBQ0MsR0FBUixDQUFZLDZCQUFaO0FBQ0EiLCJzb3VyY2VzQ29udGVudCI6WyIjIS91c3IvYmluL2VudiBub2RlXG5cbmltcG9ydCBxdWVzdGlvbk5hbWVBbmRTYXlIZWxsbyBmcm9tICcuLi9pbmRleCc7XG5cbmNvbnNvbGUubG9nKCdXZWxjb21lIHRvIHRoZSBCcmFpbiBHYW1lcyEnKTtcbnF1ZXN0aW9uTmFtZUFuZFNheUhlbGxvKCk7XG4iXX0=
